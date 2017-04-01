85c398bf9337844cb3b710f599ccec4c757ec9a2
zhangyuwei <570146800@qq.com>
2017-04-01 15:42:21 +0800

    nit db

--- app/src/main/java/com/bazinga/bazingaweather/MainActivity.java
+++ app/src/main/java/com/bazinga/bazingaweather/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.bazinga.bazingaweather;
+package com.bazinga.bazingaweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.bazinga.bazingaweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
